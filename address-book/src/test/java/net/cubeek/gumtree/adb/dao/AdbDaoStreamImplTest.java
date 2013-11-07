63acef677213a3622676426ef51f420422140a8d
cubeek <stonavsky@cubeek.net>
2013-11-07 16:12:18 +0000

    Test stubs

---
+++ address-book/src/test/java/net/cubeek/gumtree/adb/dao/AdbDaoStreamImplTest.java
@@ -0,0 +1,24 @@
+package net.cubeek.gumtree.adb.dao;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Jakub Stonavsky &lt;stonavsky&#64;cubeek.net&gt;
+ */
+public class AdbDaoStreamImplTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testFindAll() throws Exception {
+
+    }
+
+    @Test
+    public void testFindByName() throws Exception {
+
+    }
+}
---
+++ address-book/src/test/java/net/cubeek/gumtree/adb/service/AdbServiceImplTest.java
@@ -0,0 +1,29 @@
+package net.cubeek.gumtree.adb.service;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Jakub Stonavsky &lt;stonavsky&#64;cubeek.net&gt;
+ */
+public class AdbServiceImplTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCountByGender() throws Exception {
+
+    }
+
+    @Test
+    public void testGetOldestPerson() throws Exception {
+
+    }
+
+    @Test
+    public void testGetDaysOlder() throws Exception {
+
+    }
+}
