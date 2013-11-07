b3aa620ee1baa46821efc5d50f77b4c56befd99f
cubeek <stonavsky@cubeek.net>
2013-11-07 16:28:55 +0000

    Constants for split @ dao

---
+++ address-book/src/main/java/net/cubeek/gumtree/adb/dao/AdbDaoStreamConstants.java
@@ -0,0 +1,20 @@
+package net.cubeek.gumtree.adb.dao;
+
+/**
+ * Person indexes within the data file.
+ *
+ * @author Jakub Stonavsky &lt;stonavsky&#64;cubeek.net&gt;
+ */
+public class AdbDaoStreamConstants {
+
+    public static final int NAME = 0;
+
+    public static final int GENDER = 1;
+
+    public static final int DOB = 2;
+
+    private AdbDaoStreamConstants() {
+        // util class
+    }
+
+}
