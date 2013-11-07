12a526d6e38d62f3d60d59828a42cac2877b3275
cubeek <stonavsky@cubeek.net>
2013-11-07 16:38:41 +0000

    Parsing data

--- address-book/src/main/java/net/cubeek/gumtree/adb/dao/AdbDaoStreamConstants.java
+++ address-book/src/main/java/net/cubeek/gumtree/adb/dao/AdbDaoStreamConstants.java
@@ -1,11 +1,13 @@
 package net.cubeek.gumtree.adb.dao;
 
 /**
- * Person indexes within the data file.
+ * Person indexes within the data file & constants.
  *
  * @author Jakub Stonavsky &lt;stonavsky&#64;cubeek.net&gt;
  */
 public class AdbDaoStreamConstants {
+
+    public static final String PATTERN_DOB = "dd/MM/yy";
 
     public static final int NAME = 0;
 
--- address-book/src/main/java/net/cubeek/gumtree/adb/dao/AdbDaoStreamImpl.java
+++ address-book/src/main/java/net/cubeek/gumtree/adb/dao/AdbDaoStreamImpl.java
@@ -1,12 +1,19 @@
 package net.cubeek.gumtree.adb.dao;
 
+import net.cubeek.gumtree.adb.entity.Gender;
 import net.cubeek.gumtree.adb.entity.Person;
+import org.apache.commons.lang3.Validate;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.joda.time.LocalDate;
+import org.joda.time.format.DateTimeFormat;
 
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
+import static net.cubeek.gumtree.adb.dao.AdbDaoStreamConstants.*;
 
 /**
  * Address book dao that reads data from a flat, comma separated file.
@@ -15,12 +22,19 @@ import java.util.List;
  */
 public class AdbDaoStreamImpl implements AdbDao {
 
+    private final List<Person> entries;
+
     /**
      * Create Dao and initialize the database
      *
-     * @param is database {@link java.io.InputStream}
+     * @param is database {@link InputStream}
      */
     public AdbDaoStreamImpl(@NotNull InputStream is) {
+        Validate.notNull(is, "Address book InputStream cannot be null!");
+
+        this.entries = new ArrayList<Person>();
+
+        reload(is);
     }
 
     @NotNull
@@ -33,6 +47,40 @@ public class AdbDaoStreamImpl implements AdbDao {
     @Override
     public Person findByName(@Nullable String name) {
         return null;
+    }
+
+    /**
+     * Reload {@link AdbDaoStreamImpl#entries} from {@link InputStream}
+     *
+     * @param is the stream
+     */
+    private void reload(InputStream is) {
+        entries.clear();
+
+        final Scanner scanner = new Scanner(is);
+        while (scanner.hasNextLine()) {
+            entries.add(parseEntryLine(scanner.nextLine()));
+        }
+    }
+
+    /**
+     * Parse the {@link net.cubeek.gumtree.adb.entity.Person} object from a comma separated data line
+     *
+     * @param line data line
+     * @return {@link net.cubeek.gumtree.adb.entity.Person} instance
+     */
+    private Person parseEntryLine(String line) {
+        final String data[] = line.split(",");
+
+        if (data == null || !(data.length == 3))
+            throw new IllegalArgumentException("Incorrect data format found for line: " + line);
+
+        final Person person = new Person();
+        person.setName(data[NAME].trim());
+        person.setGender(Gender.findByKey(data[GENDER].trim()));
+        person.setDob(LocalDate.parse(data[DOB].trim(), DateTimeFormat.forPattern(PATTERN_DOB)));
+
+        return person;
     }
 
 }
